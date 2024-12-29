cc2d8e5af0bed98e4da823a8e09bf9c1df1a02e3
Bri A <alexiellis22@gmail.com>
2024-12-29 16:45:32 -0500

    Do experiment section and comment out code/Update README.md

--- js/index.js
+++ js/index.js
@@ -1,3 +1,29 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/* let iceCreamFlavor = ["cookiesNcream", "chocolate", "vanilla", "blueberryCheeseCake"];
+console.log(iceCreamFlavor);
+let testScores = [70, 80, 55, 95];
+console.log(iceCreamFlavor);
+console.log(testScores);
+
+// Remove an item from your arrays
+
+iceCreamFlavor.shift();        // Remove first item "cookiesNcream" from array
+console.log(iceCreamFlavor);   //Output: [ 'chocolate', 'vanilla', 'blueberryCheeseCake' ]
+testScores.pop();              // Remove last item 95 from array
+console.log(testScores);       //Output: [ 70, 80, 55 ]
+
+//Add an item to your arrays
+
+iceCreamFlavor.push("eggnog") // add "eggnog" to array
+console.log(iceCreamFlavor);  //Output: [ 'chocolate', 'vanilla', 'blueberryCheeseCake', 'eggnog' ]
+testScores.push(100)         // add 100 to array
+console.log(testScores);  //Output: [ 70, 80, 55, 100 ]
+
+console.log(typeof(testScores)); //Output: object
+
+let mixedDataTypes = [99, "red", "balloons🎈"]; //  accepts different datatypes
+console.log(mixedDataTypes);   // Output: [ 99, 'red', 'balloons🎈' ]
+ */
